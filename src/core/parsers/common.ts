7f1ca2476e048a21d7de63e106d7bbe8594cf9b1
Eridanus Sora <sora@sound.moe>
2020-06-28 15:18:56 +0800

    #45, use given hex key as default

--- src/core/parsers/common.ts
+++ src/core/parsers/common.ts
@@ -10,52 +10,54 @@ export default class Parser {
         return new Promise(async (resolve, reject) => {
             if (downloader.m3u8.isEncrypted) {
                 const keys = {};
-                keys[CommonUtils.buildFullUrl(downloader.m3u8.m3u8Url, downloader.m3u8.key)] = '';
+                keys[CommonUtils.buildFullUrl(downloader.m3u8.m3u8Url, downloader.m3u8.key)] = downloader.key || '';
                 // collect all key urls
                 for (const chunk of downloader.m3u8.chunks) {
                     if (chunk.key && !keys[chunk.key]) {
-                        keys[CommonUtils.buildFullUrl(downloader.m3u8.m3u8Url, chunk.key)] = '';
+                        keys[CommonUtils.buildFullUrl(downloader.m3u8.m3u8Url, chunk.key)] = downloader.key || '';
                     }
                 }
                 // download all keys
-                let counter = 1;
-                for (const url of Object.keys(keys)) {
-                    downloader.Log.info(`Downloading decrypt keys. (${counter} / ${Object.keys(keys).length})`);
-                    let response;
-                    const options: AxiosRequestConfig = {};
-                    if (downloader.cookies) {
-                        options.headers = {
-                            'Cookie': downloader.cookies
-                        };
-                    }
-                    if (Object.keys(downloader.headers).length > 0) {
-                        options.headers = downloader.headers;
-                    }
-                    if (downloader.proxy) {
-                        response = await requestRaw(url, {
-                            host: downloader.proxyHost,
-                            port: downloader.proxyPort
-                        },  {
+                if (!downloader.key) {
+                    let counter = 1;
+                    for (const url of Object.keys(keys)) {
+                        downloader.Log.info(`Downloading decrypt keys. (${counter} / ${Object.keys(keys).length})`);
+                        let response;
+                        const options: AxiosRequestConfig = {};
+                        if (downloader.cookies) {
+                            options.headers = {
+                                'Cookie': downloader.cookies
+                            };
+                        }
+                        if (Object.keys(downloader.headers).length > 0) {
+                            options.headers = downloader.headers;
+                        }
+                        if (downloader.proxy) {
+                            response = await requestRaw(url, {
+                                host: downloader.proxyHost,
+                                port: downloader.proxyPort
+                            },  {
+                                    responseType: 'arraybuffer',
+                                    ...options
+                                },
+                            );
+                        } else {
+                            response = await requestRaw(url, null, {
                                 responseType: 'arraybuffer',
                                 ...options
-                            },
-                        );
-                    } else {
-                        response = await requestRaw(url, null, {
-                            responseType: 'arraybuffer',
-                            ...options
-                        });
+                            });
+                        }
+                        const hexKey =
+                            Array.from(
+                                new Uint8Array(response.data)
+                            ).map(
+                                i => i.toString(16).length === 1 ? '0' + i.toString(16) : i.toString(16)
+                            ).join('');
+                        downloader.saveEncryptionKey(CommonUtils.buildFullUrl(
+                            downloader.m3u8.m3u8Url, url
+                        ), hexKey);
+                        counter++;
                     }
-                    const hexKey =
-                        Array.from(
-                            new Uint8Array(response.data)
-                        ).map(
-                            i => i.toString(16).length === 1 ? '0' + i.toString(16) : i.toString(16)
-                        ).join('');
-                    downloader.saveEncryptionKey(CommonUtils.buildFullUrl(
-                        downloader.m3u8.m3u8Url, url
-                    ), hexKey);
-                    counter++;
                 }
                 resolve({});
             } else {
