1f6382c9587e15ace0e5c4cb99b60749f047e587
fireattack <human.peng@gmail.com>
2021-04-02 17:33:58 -0500

    Simplify URL joiner (and fix #71)
    No need to reinvent the wheel

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,18 +2,7 @@ import { URL } from "url";
 
 class CommonUtils {
     static buildFullUrl(host: string, path: string) {
-        if (path.startsWith("http")) {
-            return path;
-        } else if (path.startsWith("//")) {
-            return new URL(host).protocol + path;
-        } else if (path.startsWith("/")) {
-            return host.match(/(htt(p|ps):\/\/.+?\/)/)[1] + path.slice(1);
-        } else if (path.startsWith("./")) {
-            const pathWithoutParams = new URL(host).origin + new URL(host).pathname;
-            return `${pathWithoutParams.split("/").slice(0, -1).join("/")}/${path.slice(2)}`;
-        } else {
-            return host.match(/(.+\/)/)[1] + path;
-        }
+        return new URL(path, host).href;
     }
 }
 
